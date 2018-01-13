55dc765232c15d16d1056c10721c745685260060
superpi15 <VMCentOS-00@VMCentOS-00.(none)>
2018-01-14 05:32:44 +0800

    On branch master 	modified:   qdfa.cpp

--- qdfa.cpp
+++ qdfa.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <map>
 #include <set>
+#include <cmath>
 
 class tDfa{
 public:
@@ -11,9 +12,19 @@ public:
 	typedef std::map<int,tTrans> tAdj;
 	bool delta( int state, int input, int * pDes );
 	tAdj adj;
+
+	void print( std::ostream& ostr){
+		for( tAdj::iterator iItr = adj.begin(); iItr != adj.end(); iItr++ ){
+			tTrans& Paths = iItr->second;
+			for( tTrans::iterator jItr = Paths.begin(); 
+				jItr != Paths.end(); jItr++ ){
+				printf("%d %d %d\n",iItr->first, jItr->first, jItr->second);
+			}
+		}
+	}
 };
 
-class tRdfa{
+class tRdfa: public tDfa{
 public:
 	;
 };
@@ -24,12 +35,16 @@ public:
 	typedef std::map<int,tFrom> tSrcLog;
 	std::set<int> AlphaBet;
 	int maxSrcNum;
+	int ExtraBitNum;
+	int AlphaBetBitNum;
 	tSrcLog SrcLog; 	// to detect multi-source
 	tDfa * pDfa;
 	int scan( tDfa& Dfa ){
 		pDfa = &Dfa;
 		AlphaBet.clear();
 		maxSrcNum = 0;
+		ExtraBitNum = 0;
+		AlphaBetBitNum = 0;
 		SrcLog.clear();
 		tDfa::tAdj::iterator iItr;
 		tDfa::tTrans::iterator jItr;
@@ -54,6 +69,35 @@ public:
 					fitr->second.size(): maxSrcNum;
 			}
 		}
+		AlphaBetBitNum = log(AlphaBet.size())/log(2);
+		ExtraBitNum = log(maxSrcNum)/log(2);
+		return 1;
+	}
+
+	tRdfa * convert(){
+		tRdfa * pRdfa;
+		pRdfa = new tRdfa;
+		for( tSrcLog::iterator itr = SrcLog.begin(); itr != SrcLog.end(); itr++  ){
+			int des = itr->first;
+			tFrom& From = itr->second;
+			for( tFrom::iterator fitr = From.begin();
+				fitr != From.end(); fitr++ ){
+				if( fitr->second.size()<2 )
+					continue;
+				int input = fitr->first;
+				int counter = 0;
+				std::set<int>::iterator srcItr;
+				for( srcItr = fitr->second.begin(); 
+					srcItr != fitr->second.end(); 
+					srcItr++, counter++ ){
+					int ExtInput;
+					ExtInput = input + (counter<<AlphaBetBitNum);
+					pRdfa->adj[*srcItr];
+					pRdfa->adj[*srcItr][ExtInput] = des;
+				}
+			}
+		}
+		return pRdfa;
 	}
 };
 
@@ -81,8 +125,12 @@ int main( int argc, char * argv[] ){
 		return 0;
 	tDfa Dfa;
 	ReadDfa( argv[1], Dfa );
+	Dfa.print( std::cout );
 	tDfaToRdfa Man;
 	Man.scan( Dfa );
+	tRdfa * pRdfa = Man.convert();
+	pRdfa->print( std::cout );
+	delete pRdfa;
 	printf("AlphaBet# %d\n", Man.AlphaBet.size() );
 	printf("maxSrcNum %d\n", Man.maxSrcNum );
 	//tRevNtk RevNtk;
