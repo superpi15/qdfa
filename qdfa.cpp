91aaf4eb7457717af04cc5ad2481ff2ef4c433aa
superpi15 <VMCentOS-00@VMCentOS-00.(none)>
2018-01-14 03:42:19 +0800

    On branch master  Changes to be committed: 	modified:   qdfa.cpp

--- qdfa.cpp
+++ qdfa.cpp
@@ -3,12 +3,58 @@
 #include <sstream>
 #include <fstream>
 #include <map>
+#include <set>
 
 class tDfa{
 public:
+	typedef std::map<int,int> tTrans;
+	typedef std::map<int,tTrans> tAdj;
 	bool delta( int state, int input, int * pDes );
-	typedef std::map<int,int> transition;
-	std::map<int,transition> adj;
+	tAdj adj;
+};
+
+class tRdfa{
+public:
+	;
+};
+
+class tDfaToRdfa{
+public:
+	typedef std::map<int,std::set<int> > tFrom;
+	typedef std::map<int,tFrom> tSrcLog;
+	std::set<int> AlphaBet;
+	int maxSrcNum;
+	tSrcLog SrcLog; 	// to detect multi-source
+	tDfa * pDfa;
+	int scan( tDfa& Dfa ){
+		pDfa = &Dfa;
+		AlphaBet.clear();
+		maxSrcNum = 0;
+		SrcLog.clear();
+		tDfa::tAdj::iterator iItr;
+		tDfa::tTrans::iterator jItr;
+		for( iItr = pDfa->adj.begin(); iItr != pDfa->adj.end(); iItr++ ){
+			int src;
+			src = iItr->first;
+			tDfa::tTrans& Paths =  iItr->second;
+			for( jItr = Paths.begin(); jItr != Paths.end(); jItr++ ){
+				int input, des;
+				input 	= jItr->first;
+				des 	= jItr->second;
+				AlphaBet.insert(input);
+				SrcLog[des];
+				SrcLog[des][input].insert(src);
+			}
+		}
+		for( tSrcLog::iterator itr = SrcLog.begin(); itr != SrcLog.end(); itr++  ){
+			tFrom& From = itr->second;
+			for( tFrom::iterator fitr = From.begin();
+				fitr != From.end(); fitr++ ){
+				maxSrcNum =  fitr->second.size()>maxSrcNum?
+					fitr->second.size(): maxSrcNum;
+			}
+		}
+	}
 };
 
 int ReadDfa( const char * FileName, tDfa& Dfa ){
@@ -35,6 +81,10 @@ int main( int argc, char * argv[] ){
 		return 0;
 	tDfa Dfa;
 	ReadDfa( argv[1], Dfa );
+	tDfaToRdfa Man;
+	Man.scan( Dfa );
+	printf("AlphaBet# %d\n", Man.AlphaBet.size() );
+	printf("maxSrcNum %d\n", Man.maxSrcNum );
 	//tRevNtk RevNtk;
 	//ReversibleBasic( Spec, RevNtk );
 	//RevNtk.print(std::cout);
