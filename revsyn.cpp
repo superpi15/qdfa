7a5d80b6ba5530d7ba6cef134ea1bfbb86b16cc0
nhspc <superpi152@gmail.com>
2018-01-13 20:24:38 +0800

    Changes to be committed: 	modified:   revsyn.cpp

--- revsyn.cpp
+++ revsyn.cpp
@@ -8,14 +8,35 @@ public:
 	tSpec():nBit(0){}
 };
 int ReadSpec( const char * FileName, tSpec& Spec ){
-	std::ifstream in( argv[1], std::ios::in );
-	
+	std::ifstream in( FileName, std::ios::in );
+	std::string line;
+	int nLine;
+	for( nLine = 0; in>>line; nLine++ ){
+		if( nLine == 0 ){
+			Spec.nBit = line.size();
+			Spec.clear();
+			Spec.resize( 1<<Spec.nBit );
+		}
+		if( line.size() != Spec.nBit ){
+			printf("Error: inconsistent width at line %d.\n", Spec.nBit );
+			return 0;
+		}
+		Spec[nLine].resize( Spec.nBit );
+		for( int i = 0; i<line.size(); i++ ){
+			if( line[i] != '0' && line[i] != '1' ){
+				printf("Error: illegal bit at line %d.\n", nLine );
+				return 0;
+			}
+			Spec[nLine][i] = (line[i] == '0') ? '0': '1';
+		}
+	}
 	in.close();
-	return 0;
+	return 1;
 }
 
 int main( int argc, char * argv[] ){
 	if( argc!=2 )
 		return 0;
-	
+	tSpec Spec;
+	ReadSpec( argv[1], Spec );
 }
